1cc23b44674180522a5278d1e5041454661a7613
unknown <kalyanipande752@gmail.com>
2026-06-10 16:53:17 +0530

    Day 3: Added Header componentLaying the foundation

--- App.js
+++ App.js
@@ -1,33 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//import { createRoot } from 'react-dom/client';
-const heading = React.createElement(
-  "h1",
-  {
-    id: "title",
-  },
-  "Heading 1"
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX😊
+  </h1>
 );
-const heading2 = React.createElement(
-  "h2",
-  {
-    id: "title",
-  },
-  "Heading 2"
-);
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-  },
-  [heading, heading2]
-);
-console.log(heading);
+
+//React functional component
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+   <h1 className="heading">Namaste React functional component</h1>
+  </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-//const root = createRoot(document.getElementById('root')); 
-//passing a react element inside the root
-//async defer
-root.render(container);
-
- // Use named import from /client
-
+root.render(<HeadingComponent/>);
